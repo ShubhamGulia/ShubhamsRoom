f9492e463e7a27d7261941c2a7b6d657944d736e
Shubham Gulia <shubhamgulia2011@gmail.com>
2018-10-30 00:38:26 -0400

    sorting and counting the frequency.

--- WordCountFreq.py
+++ WordCountFreq.py
@@ -14,7 +14,7 @@ def start(url):
             word_list.append(each_word)
     clean_word(word_list)
 
-# cleaning words which has symbols 
+# cleaning words which has symbols
 def clean_word(word_list):
     clean_word_list=[]
     for word in word_list:
@@ -22,10 +22,18 @@ def clean_word(word_list):
         for m in range(0, len(symbols)):
             word = word.replace(symbols[m],"")
         if len(word) > 0:
-            print(word)
+            #print(word)
             clean_word_list.append(word)
+    create_dictionary(clean_word_list)
 
-
-
+def create_dictionary(clean_word_list):
+    word_count = {}
+    for words in clean_word_list:
+        if words in word_count:
+            word_count[words] += 1
+        else:
+            word_count[words] = 1
+    for key, value in sorted(word_count.items(), key =operator.itemgetter(0)):
+        print(key,value)
 
 start('https://web.njit.edu/~rr524/about.html')
